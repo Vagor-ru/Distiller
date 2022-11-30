d91fc0a82f9fd5f2a2c32e5167637e5c55e2e5db
v.a.gor <gorbatyh@yandex.ru>
2022-11-30 18:39:38 +0800

    исправлен перенос конфига

--- Distiller/setupDistiller.sh
+++ Distiller/setupDistiller.sh
@@ -3,19 +3,19 @@
 # Запуск: bash setupDistiller.sh
 
 #Переименование предыдущего конфигурационного файла
-mv -f /home/pi/Distiller/configDistiller.json /home/pi/Distiller/preconfigDistiller.json
+mv -f "/home/pi/Distiller/configDistiller.json" "/home/pi/Distiller/preconfigDistiller.json"
 
 #Распаковка архива
-unzip -u Distiller.zip -d /home/pi/Distiller
+unzip -u "Distiller.zip" -d "/home/pi/Distiller"
 
 
 #Создание виртуального окружения
-python3 -m venv /home/pi/Distiller/env
+python3 -m venv "/home/pi/Distiller/env"
 
 #Активация и наполнение виртуального окружения
-cd /home/pi/Distiller
-source env/bin/activate
+cd "/home/pi/Distiller"
+source "env/bin/activate"
 pip install -r requirements.txt
-mv /home/pi/Desktop/startDistiller.sh /home/pi/Distiller
-rm /home/pi/Desktop/setupDistiller.sh
-rm /home/pi/Desktop/Distiller.zip
+mv "/home/pi/Desktop/startDistiller.sh /home/pi/Distiller"
+rm "/home/pi/Desktop/setupDistiller.sh"
+rm "/home/pi/Desktop/Distiller.zip"
