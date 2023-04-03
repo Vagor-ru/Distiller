2d8e3a0bf0a5dc76524e6cbc6a2fe6e5d33b7098
v.a.gor <gorbatyh@yandex.ru>
2023-04-03 22:03:37 +0800

    Небольшие правки

--- Distiller/Distiller/helpers/deletingOldLogs.py
+++ Distiller/Distiller/helpers/deletingOldLogs.py
@@ -1,10 +1,11 @@
-import os
 import time
+import os
 
 def DeletingOldLogs(directory='logs', days=7):
-    with scandir(directory) as it:
+    with os.scandir(directory) as it:
         for entry in it:
             f=os.path.join(directory,entry.name)
             if os.stat(f).st_mtime < time.time() - days * 86400:
                 os.remove(f)
+                print(f'{f} deleted')
     pass
