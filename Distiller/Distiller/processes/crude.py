4e6929d78987fcc0789b54fc2939c002c8bda15c
v.a.gor <gorbatyh@yandex.ru>
2023-04-24 16:24:54 +0800

    Уточнение порогов прогрева перед головами

--- Distiller/Distiller/processes/crude.py
+++ Distiller/Distiller/processes/crude.py
@@ -95,13 +95,13 @@ class Crude(threading.Thread):
         # Новый набор кнопок
         self.pageUpdate(None, 'ABORT_NEXT.html')
         power.value = power.Pmax
-        while thermometers.getValue('Верх') < config['PARAMETERS']['T_Head']['value']-5:
+        while thermometers.getValue('Верх') < config['PARAMETERS']['T_Head']['value']-6:
             # Вывести на дисплей состояние
             sec=int(int(time.time()-tBgn))
             sec_str=u'{:02}:{:02}'\
                .format((sec//60)%60, sec%60)
             self.pageUpdate('2-й перегон: прогрев колонны %s<br>%s'%(sec_str,self.Duration()))
-            if thermometers.getValue('Верх') > config['PARAMETERS']['T_Head']['value']-12:
+            if thermometers.getValue('Верх') > config['PARAMETERS']['T_Head']['value']-14:
                 power.value=config['PARAMETERS']['P_H2O']['value']-config['PARAMETERS']['Kp']['value']*\
                     (config['PARAMETERS']['T_H2O']['value']-thermometers.getValue('Низ'))
             # При получении команды прервать процесс
